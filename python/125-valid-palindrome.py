3611f5bb706395a7929d18d6af364869e774f43a
Hao Chen <hach@microsoft.com>
2016-08-25 00:46:03 -0700

    Create 125-valid-palindrome.py

---
+++ python/125-valid-palindrome.py
@@ -0,0 +1,20 @@
+# source: https://leetcode.com/problems/valid-palindrome/
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            else:
+                if s[l].lower() !=s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+        return True
