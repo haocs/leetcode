dbce8f240da257e5e82f317d5b43f0053026a24a
Hao Chen <hach@microsoft.com>
2016-08-24 23:59:00 -0700

    Create 33-search-in-rotated-sorted-array.py

---
+++ python/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,25 @@
+# source: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return l if nums[l] == target else -1
